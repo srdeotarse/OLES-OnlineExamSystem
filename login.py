0667a5b0c128ae5a9903f4e6da6b7853cb3c751b
AmayTripathi18 <amaytripathi16@gmail.com>
2021-03-02 13:30:12 +0530

    INSERT DATA IN DB

--- login.py
+++ login.py
@@ -13,6 +13,7 @@ class Login(QDialog):
         loadUi("login.ui",self)
         self.signframe.setVisible(False)
         self.loginbtn.clicked.connect(self.loginFunction)
+        self.signbtn.clicked.connect(self.signup)
         self.blur()
         self.accountbtn.clicked.connect(self.createAccount)
         self.accountbtn_2.clicked.connect(self.loginAccount)
@@ -21,8 +22,8 @@ class Login(QDialog):
     def loginFunction(self):
         rollno = self.rollno.text()
         password = self.password.text()
-        #print("Successfully logged in with rollno - ",rollno,"and password - ",password)
-        self.connectDB()
+        print("Successfully logged in with rollno - ",rollno,"and password - ",password)
+
 
     def createAccount(self):
         self.loginframe.setVisible(False)
@@ -42,15 +43,38 @@ class Login(QDialog):
         print(fname)
         self.photolbl.setPixmap(QPixmap(fname))
 
+    def signup(self):
+
+        print("hiaaa")
+        self.connectDB()
+
+
+
     def connectDB(self):
         try:
-            connection = mysql.connector.connect(host='sql12.freesqldatabase.com',database='sql12394903',user='sql12394903',password='WNRjtUUQWr',port='3306')
-            mySql_Create_Table_Query = """SELECT * FROM user"""
+            connection = mysql.connector.connect(host='sql12.freesqldatabase.com', database='sql12394903',
+                                                 user='sql12394903', password='WNRjtUUQWr', port='3306')
             cursor = connection.cursor()
-            cursor.execute(mySql_Create_Table_Query)
-            result = cursor.fetchall()
-            for x in result:
-                print(x)
+
+            name = self.name.text()
+            email = self.email.text()
+            password = self.password_2.text()
+            rollno = self.rollno_2.text()
+            mySql_Create_Table_Query = " insert into user (name,email,password,rollno) VALUES(%s,%s,%s,%s)"
+            value =(name,email,password,rollno)
+            if(name=='' or email=='' or password=='' or rollno==''):
+                m = QMessageBox.about(self, "warning","Pls fill all data")
+
+            else:
+                cursor.execute(mySql_Create_Table_Query, value)
+                connection.commit()
+                print("Data inserted")
+
+
+
+            # result = cursor.fetchall()
+            # for x in result:
+            #     print(x)
 
         except mysql.connector.Error as error:
             print("Failed to create table in MySQL: {}".format(error))
@@ -59,7 +83,35 @@ class Login(QDialog):
             if connection.is_connected():
                 cursor.close()
                 connection.close()
-                print("MySQL connection is closed")     
+                print("MySQL connection is closed")
+            # print("hello")
+            # connection = mysql.connector.connect(host='sql12.freesqldatabase.com',database='sql12394903',user='sql12394903',password='WNRjtUUQWr',port='3306')
+            # cursor = connection.cursor()
+            # print("hi")
+
+            sql1 = """SELECT * FROM user"""
+
+           # sql = ("INSERT INTO user (name,email,password,type,photo,rollno) VALUES(%s,%s,%s,%s,%s,%s)",
+            #       (self.name.get(),
+             #       self.email.get(),
+              #      self.password_2.get(),
+               #     self.type.get(),
+                #    self.rollno_2.get()
+                 #   ))
+
+        #     cursor.execute(sql1)
+        #     result = cursor.fetchall()
+        #     for x in result:
+        #         print(x)
+        #     connection.commit()
+        #     connection.close()
+        #     messagebox.showinfo("Sucesss",parent = self.root)
+        #     print("MySQL connection is closed with data inserted")
+        #
+        # except mysql.connector.Error as error:
+        #     print("Failed to create table in MySQL: {}".format(error))
+        #
+        #
 
 app = QApplication(sys.argv)
 mainwindow = Login()
